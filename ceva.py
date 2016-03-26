2819267ff788ce1cc74dd36c0688fc99e2c135df
rawcode <andreeaciobica@gmail.com>
2016-03-26 14:03:04 +0200

    My Django Girls app, first commit

---
+++ ceva.py
@@ -0,0 +1,16 @@
+print ('Hello, Django girls')
+def blue():
+	print('My head hurts!')
+	print("I'm fine now!")
+blue()
+def hi(name): 
+    print ('Hello ' + name + '!')
+
+girls = ['Georgiana', 'Sanda', 'Andreea']
+for name in girls:
+	hi(name)
+	print('Next girl')
+	
+for i in range(1, 6):
+	print(i)
+
